201a9f99d436159826f60845c2c57b216e4180d0
hudulgi <jeonghu@gmail.com>
2020-03-23 22:54:29 +0900

    장 시가 반영, 휴일 무한루프 돌지 않도록 수정

--- get_op.py
+++ get_op.py
@@ -5,17 +5,26 @@ import time
 cr = Creon.Creon()
 targetTable = pd.read_csv('C://Users//JHOO//iCloudDrive//stock//stock_data//target_list.csv')
 
-code_list = targetTable['code']
-openPrice = []
-name = []
+code_list = targetTable['code'].to_list()
+code_list2 = code_list[:]
 
-for code in code_list:
-    print(code)
-    openPrice.append(cr.getOpen(code))
-    name.append(cr.getName(code))
-    time.sleep(0.2)
+resultDf = pd.DataFrame(columns=['code', 'name', 'open'])
 
-targetTable['open'] = openPrice
-targetTable['name'] = name
+while code_list2:
+    for code in code_list:
+        if code not in code_list2:
+            continue
+        op, name, marketFlag = cr.get_open(code)
+        print(code, name, op, marketFlag)
 
-targetTable.to_csv('C://Users//JHOO//iCloudDrive//stock//stock_data//target_list2.csv', index = False)
+        if marketFlag == '2':
+            temp = pd.Series([code, name, op], index=['code', 'name', 'open'])
+            resultDf = resultDf.append(temp, ignore_index=True)
+            code_list2.remove(code)
+        elif marketFlag == '4':
+            code_list2.remove(code)
+
+        time.sleep(0.3)
+
+print(resultDf)
+#targetTable.to_csv('C://Users//JHOO//iCloudDrive//stock//stock_data//target_list2.csv', index = False)
