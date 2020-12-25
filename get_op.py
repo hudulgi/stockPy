03990f76bb67f274a41de8ae54c6384078470b5a
hudulgi <jeonghu@gmail.com>
2020-12-25 23:02:44 +0900

    파일 경로 수정

--- get_op.py
+++ get_op.py
@@ -11,17 +11,19 @@ def date_split(line):
     return date2
 
 
+data_path = "C:\\CloudStation\\dt_data"
+
 bot.sendMessage(myId, "시가 불러오기를 시작합니다.")
 
 finishCode = 0
 
 cr = Creon.Creon()
 
-targetTable = pd.read_csv('C://Users//JHOO//iCloudDrive//stock//stock_data//target_list.csv')
+targetTable = pd.read_csv(data_path + '\\target\\target_list.csv')
 code_list = targetTable['code'].to_list()
 code_list2 = code_list[:]
 
-ignoreItem = pd.read_csv('C://Users//JHOO//iCloudDrive//stock//stock_data//ignore_item.csv')
+ignoreItem = pd.read_csv(data_path + '\\target\\ignore_item.csv')
 nowDate = datetime.datetime.now().date()
 for item in ignoreItem.iterrows():
     if item[1]['code'] not in code_list:
@@ -72,6 +74,6 @@ while code_list2:
         finishCode = 1
 
 print(resultDf)
-resultDf.to_csv('C://Users//JHOO//iCloudDrive//stock//stock_data//open_price//op_' + nowDate.strftime("%y%m%d") +
+resultDf.to_csv(data_path + '\\daily_data\\open_price\\op_' + nowDate.strftime("%y%m%d") +
                 '.csv', index=False)
 bot.sendMessage(myId, "시가 불러오기를 완료하였습니다. 종료코드:%i" % finishCode)
