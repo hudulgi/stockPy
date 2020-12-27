35f99d46e929d3d310647c46435479154ff1bcfa
hudulgi <jeonghu@gmail.com>
2020-12-27 22:12:03 +0900

    파일 경로 정보 수정

--- get_op.py
+++ get_op.py
@@ -11,7 +11,9 @@ def date_split(line):
     return date2
 
 
-data_path = "C:\\CloudStation\\dt_data"
+base_path = "C:\\CloudStation\\dt_data"
+data_path = base_path + "\\target"
+open_path = base_path + "\\daily_data\\open_price"
 
 bot.sendMessage(myId, "시가 불러오기를 시작합니다.")
 
@@ -19,11 +21,11 @@ finishCode = 0
 
 cr = Creon.Creon()
 
-targetTable = pd.read_csv(data_path + '\\target\\target_list.csv')
+targetTable = pd.read_csv(data_path + '\\target_list.csv')
 code_list = targetTable['code'].to_list()
 code_list2 = code_list[:]
 
-ignoreItem = pd.read_csv(data_path + '\\target\\ignore_item.csv')
+ignoreItem = pd.read_csv(data_path + '\\ignore_item.csv')
 nowDate = datetime.datetime.now().date()
 for item in ignoreItem.iterrows():
     if item[1]['code'] not in code_list:
@@ -74,6 +76,5 @@ while code_list2:
         finishCode = 1
 
 print(resultDf)
-resultDf.to_csv(data_path + '\\daily_data\\open_price\\op_' + nowDate.strftime("%y%m%d") +
-                '.csv', index=False)
+resultDf.to_csv(open_path + '\\op_' + nowDate.strftime("%y%m%d") + '.csv', index=False)
 bot.sendMessage(myId, "시가 불러오기를 완료하였습니다. 종료코드:%i" % finishCode)
